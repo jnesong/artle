5664b68d134a9420b753809d8586e0a8e60fb1d3
Jenny Chau <jnechau@gmail.com>
2022-05-18 23:52:01 -0400

    fetching from Art Institute of Chicago API

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+//libraries
+import {useEffect, useState} from 'react';
+//gallery
+import a0 from './gallery/a0.jpg';
 
 function App() {
+  const [chicago, setChicago] = useState();
+
+  useEffect(() => {
+
+    fetch ("https://api.artic.edu/api/v1/artworks/27992?fields=id,title,image_id")
+    .then (r=>r.json())
+    .then (data => setChicago(data))
+
+  }, []);
+
+  // const imageObj = URL.createObjectURL(img)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      <img src={a0} alt="artwork" />
+      <p> Hello </p>
+      <img src={chicago} alt="fetched?" />
+    </div >
   );
 }
 
