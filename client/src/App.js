af5bb27b7c28eb819ad9ef94d507552b6644eaab
Jenny Chau <jnechau@gmail.com>
2022-05-19 11:42:35 -0400

    sucessfully fetched artist title and all artwork data from AIOC API

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import a0 from './gallery/a0.jpg';
 function App() {
   const [chicago, setChicago] = useState();
   const [imageURL, setImageURL] = useState();
+  const [artist, setArtist] = useState();
 
   useEffect(() => {
 
@@ -15,22 +16,34 @@ function App() {
 
   }, []);
 
-  // if (chicago) {console.log(chicago)}
+  if (chicago) { console.log(chicago.data.id) }
   // if (chicago) {console.log(chicago.config.iiif_url)}
   // if (chicago) {console.log(chicago.data.image_id)}
   // if (chicago) {console.log(chicago.config.iiif_url + "/" + chicago.data.image_id + "/full/843,/0/default.jpg" )}
 
   useEffect(() => {
-    if (chicago) {setImageURL(chicago.config.iiif_url + "/" + chicago.data.image_id + "/full/843,/0/default.jpg")}
+    if (chicago) { setImageURL(chicago.config.iiif_url + "/" + chicago.data.image_id + "/full/843,/0/default.jpg") }
   }, [chicago])
 
+  useEffect(() => {
+    if (chicago) {
+      fetch("https://api.artic.edu/api/v1/artworks/" + chicago.data.id)
+        .then(r => r.json())
+        .then(chicagoData => setArtist(chicagoData))
+    }
+  }, [chicago])
 
+  console.log(artist.data.artist_title)
+
+  let thisArtist = ""
+
+  if (artist) {thisArtist = artist.data.artist_title}
 
   return (
     <div>
       <img src={a0} alt="artwork" />
       <img src={imageURL} alt="artwork" />
-
+      <p> {thisArtist} </p>
     </div >
   );
 }
