b266e19588b265229aad0a49a1e58da210b51c66
Jenny Chau <jnechau@gmail.com>
2022-05-19 00:02:19 -0400

    successfully fetched json data from Art Institute of Chicago API

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 //libraries
-import {useEffect, useState} from 'react';
+import { useEffect, useState } from 'react';
 //gallery
 import a0 from './gallery/a0.jpg';
 
@@ -8,19 +8,18 @@ function App() {
 
   useEffect(() => {
 
-    fetch ("https://api.artic.edu/api/v1/artworks/27992?fields=id,title,image_id")
-    .then (r=>r.json())
-    .then (data => setChicago(data))
+    fetch("https://api.artic.edu/api/v1/artworks/27992?fields=id,title,image_id")
+      .then(r => r.json())
+      .then(data => setChicago(data))
 
   }, []);
 
-  // const imageObj = URL.createObjectURL(img)
+  console.log(chicago)
+
 
   return (
     <div>
       <img src={a0} alt="artwork" />
-      <p> Hello </p>
-      <img src={chicago} alt="fetched?" />
     </div >
   );
 }
