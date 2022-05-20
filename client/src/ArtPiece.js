acb43f169a4f9a61ff6f6cb9f1866582601e8655
Jenny Chau <jnechau@gmail.com>
2022-05-20 00:14:09 -0400

    try one black out done

--- client/src/ArtPiece.js
+++ client/src/ArtPiece.js
@@ -19,25 +19,43 @@ const ArtPiece = () => {
     };
 
     //3 lines - the style height of the div try-one to the current art piece's height
-    let tryOneStyle = {
+    let gridContainerStyle = {
         height: pieceHeight
     }
 
     //7 lines - updates height of div try-one if the image height/art piece is changed
     useEffect(() => {
         if (pieceHeight) {
-            tryOneStyle = {
+            gridContainerStyle = {
                 height: "pieceHeight px"
             }
         }
     }, [pieceHeight])
 
-    console.log(tryOneStyle)
+    const tryOne = {
+        opacity: "0%"
+    }
+
 
     return (
         <div className="art-div">
-            <div className="try-one" style={tryOneStyle}>
-                <div className="item-a"/>
+            <div className="grid-container" style={gridContainerStyle}>
+                <div className="item-a" />
+                <div className="item-b" />
+                <div className="item-c" />
+                <div className="item-d" />
+                <div className="item-e" />
+                <div className="item-f" />
+                <div className="item-g" style={tryOne}/>
+                <div className="item-h" />
+                <div className="item-i" />
+                <div className="item-j" />
+                <div className="item-k" />
+                <div className="item-l" />
+                <div className="item-m" />
+                <div className="item-n" />
+                <div className="item-o" />
+                <div className="item-p" />
             </div>
             <img onLoad={onPieceLoad} className="art-piece" src={a1} alt="artwork" />
         </div>
