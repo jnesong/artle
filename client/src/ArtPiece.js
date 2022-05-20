1195818885f8dc58a2d91ede5717996e86341c7d
Jenny Chau <jnechau@gmail.com>
2022-05-19 23:44:43 -0400

    first block done, grid created

--- client/src/ArtPiece.js
+++ client/src/ArtPiece.js
@@ -11,16 +11,6 @@ const ArtPiece = () => {
 
     const [pieceHeight, setPieceHeight] = useState(0)
 
-    // const tryOne =
-    // <>
-    //     <div className="box1"></div>
-    //     <div className="box1"></div>
-    //     <div className="box2"></div>
-    //     <div className="box3"></div>
-    //     <div className="box4"></div>
-    //     <div className="box5"></div>
-    // </>
-
     //5 lines - get the current images height in px on image load and sets it to the pieceHeight state
     const onPieceLoad = ({ target: img }) => {
         const { height } = img;
@@ -46,7 +36,9 @@ const ArtPiece = () => {
 
     return (
         <div className="art-div">
-            <div className="try-one" style={tryOneStyle}></div>
+            <div className="try-one" style={tryOneStyle}>
+                <div className="item-a"/>
+            </div>
             <img onLoad={onPieceLoad} className="art-piece" src={a1} alt="artwork" />
         </div>
     );
