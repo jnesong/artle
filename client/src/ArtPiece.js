f1eca67b7dab8aef2efb98ed2cb571fcd4ae5f85
Jenny Chau <jnechau@gmail.com>
2022-05-19 23:07:56 -0400

    pre grid attempts at art piece blackout

--- client/src/ArtPiece.js
+++ client/src/ArtPiece.js
@@ -6,12 +6,25 @@ import a0 from './gallery/a0.jpg';
 
 const ArtPiece = () => {
 
+    const tryOne =
+    <>
+        <div className="box1"></div>
+        <div className="box1"></div>
+        <div className="box2"></div>
+        <div className="box3"></div>
+        <div className="box4"></div>
+        <div className="box5"></div>
+    </>
 
+    const onImageLoad = ({target:img}) => {
+        const {height, offsetWidth} = img;
+        console.log(height);
+    };
 
     return (
         <div className="art-div">
-        <div className="boxes"></div>
-        <img className="art-piece" src={a0} alt="artwork" />
+            {tryOne}
+            <img onLoad={onImageLoad} className="art-piece" src={a0} alt="artwork" />
         </div>
     );
 
