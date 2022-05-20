dee9b24edafc3f24d4b5a9e7b76569e3270d72e6
Jenny Chau <jnechau@gmail.com>
2022-05-19 23:23:04 -0400

    successful blackout of photo, need to split into grid

--- client/src/ArtPiece.js
+++ client/src/ArtPiece.js
@@ -1,30 +1,49 @@
-//css
-import './artpiece.css'
+//libraries
+import { useState, useEffect } from 'react';
 //gallery
 import a0 from './gallery/a0.jpg';
+//css
+import './artpiece.css'
 
 
 const ArtPiece = () => {
 
-    const tryOne =
-    <>
-        <div className="box1"></div>
-        <div className="box1"></div>
-        <div className="box2"></div>
-        <div className="box3"></div>
-        <div className="box4"></div>
-        <div className="box5"></div>
-    </>
+    const [pieceHeight, setPieceHeight] = useState(0)
 
-    const onImageLoad = ({target:img}) => {
-        const {height, offsetWidth} = img;
+    // const tryOne =
+    // <>
+    //     <div className="box1"></div>
+    //     <div className="box1"></div>
+    //     <div className="box2"></div>
+    //     <div className="box3"></div>
+    //     <div className="box4"></div>
+    //     <div className="box5"></div>
+    // </>
+
+    const onPieceLoad = ({ target: img }) => {
+        const { height } = img;
         console.log(height);
+        setPieceHeight(height)
     };
+
+    let tryOneStyle = {
+        height: pieceHeight
+    }
+
+    useEffect(() => {
+        if (pieceHeight) {
+            tryOneStyle = {
+                height: "pieceHeight px"
+            }
+        }
+    }, [pieceHeight])
+
+    console.log(tryOneStyle)
 
     return (
         <div className="art-div">
-            {tryOne}
-            <img onLoad={onImageLoad} className="art-piece" src={a0} alt="artwork" />
+            <div className="try-one" style={tryOneStyle}></div>
+            <img onLoad={onPieceLoad} className="art-piece" src={a0} alt="artwork" />
         </div>
     );
 
